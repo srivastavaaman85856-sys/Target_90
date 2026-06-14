89296166ddd672838f5a7e803a98ff2edf9b3af4
Aman Srivastava <srivastavaaman.85856@gmail.com>
2026-06-14 23:41:21 +0530

    Add files via upload

---
+++ rotate matrix180.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Rotate180{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
