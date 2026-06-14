86d495ea628d7d2898759b57e797d03b0c160b5a
Aman Srivastava <srivastavaaman.85856@gmail.com>
2026-06-14 12:41:09 +0530

    Add files via upload

---
+++ matrix diagonal diff.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Diagonaldiff{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int d1 = 0;
+        int d2 = 0;
+        for(int i=0;i<n;i++){
+           d1=arr[i][i];
+           d2=arr[i][n-i-1];
+           System.out.print(d2);
+        }
+        int diff = d1-d2;
+        System.out.println(diff);
+
+    }
+}
