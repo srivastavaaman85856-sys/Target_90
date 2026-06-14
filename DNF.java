d3a84f20182a12e3eed35725bb783c0be92df8c7
Aman Srivastava <srivastavaaman.85856@gmail.com>
2026-06-14 12:40:45 +0530

    Add files via upload

---
+++ DNF.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class DNF{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int low = 0;
+        int mid = 0;
+
+        int high = n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
